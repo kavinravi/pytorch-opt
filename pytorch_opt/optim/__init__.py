246f758afcc188c48b57e2b4c5a1e47370e5a172
kavinravi <kavinravi121@gmail.com>
2026-08-26 18:24:10 -0700

    feat(optim): EKFAC (eigenbasis rescaling; exact reduction-to-KFAC and Frobenius-optimality proofs)

--- pytorch_opt/optim/__init__.py
+++ pytorch_opt/optim/__init__.py
@@ -1,4 +1,5 @@
 from .adahessian import AdaHessian
+from .ekfac import EKFAC
 from .kfac import KFAC
 from .muon import Muon
 from .ngd import NGD
@@ -7,4 +8,4 @@ from .soap import SOAP
 from .sophia import Sophia
 from .trust_ncg import TrustNCG
 
-__all__ = ["AdaHessian", "KFAC", "Muon", "NGD", "Shampoo", "SOAP", "Sophia", "TrustNCG"]
+__all__ = ["AdaHessian", "EKFAC", "KFAC", "Muon", "NGD", "Shampoo", "SOAP", "Sophia", "TrustNCG"]
